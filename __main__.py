b00b42f6ae786ae49254125e0def5b8c600d46f8
Diderik <59435489+Diderikdm@users.noreply.github.com>
2022-10-13 13:36:06 +0200

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,17 @@
+from <<path.to.module>>.unittesting.unittesting import test_unit_tests
+from <<path.to.module>>.unittesting import (
+    test_unit_testing_one,
+    test_unit_testing_two,
+    test_unit_testing_three
+)
+
+# Example usage
+def main():
+    test_unit_tests([
+        test_unit_testing_one,
+        test_unit_testing_two,
+        test_unit_testing_three
+    ])
+
+if __name__ == "__main__":
+    main()
